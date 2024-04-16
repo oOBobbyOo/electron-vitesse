fa82e156097ad005a34c3d79f1e542c56e433bd5
oOBobbyOo <944831575@qq.com>
2024-04-16 16:31:36 +0800

    refactor: change component

--- src/renderer/src/views/Hi.vue
+++ src/renderer/src/views/Hi.vue
@@ -4,6 +4,17 @@ import Versions from '../components/Versions.vue'
 </script>
 
 <template>
-  <Wellcome />
-  <Versions />
+  <div class="hi">
+    <Wellcome />
+    <Versions />
+  </div>
 </template>
+
+<style scoped lang="less">
+.hi {
+  @apply flex-center flex-col w-full h-full; 
+
+  background-image: url('../assets/wavy-lines.svg');
+  background-size: cover;
+}
+</style>
