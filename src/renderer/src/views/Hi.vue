520a7629c6fdbd6c81d3ddfc8b15260764e08dfb
oOBobbyOo <944831575@qq.com>
2024-04-16 15:19:57 +0800

    feat: router

---
+++ src/renderer/src/views/Hi.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Wellcome from '../components/Wellcome.vue'
+import Versions from '../components/Versions.vue'
+</script>
+
+<template>
+  <Wellcome />
+  <Versions />
+</template>
