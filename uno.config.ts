6de2a1eee4fb259039321d58a1f2ba2d7669564c
oOBobbyOo <944831575@qq.com>
2024-04-16 12:22:59 +0800

    feat: unocss

---
+++ uno.config.ts
@@ -0,0 +1,29 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    ['flex-center', 'flex justify-center items-center']
+  ],
+  theme: {
+    colors: {}
+  },
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {}
+    })
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()]
+})
